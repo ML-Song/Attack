2fb5a5fc7e1a5480c43ad19fc2b9566716689a06
ML-Song <sl12160010@gmail.com>
2019-04-24 20:07:39 +0800

    with noise images

--- train_non_target.py
+++ train_non_target.py
@@ -90,6 +90,7 @@ if __name__ == '__main__':
     beta = 8
     batch_size = 32
     num_classes = 110
+    interval = 10
     checkpoint_dir = 'saved_models'
     devices = [3]
     comment = 'model: {}, with_target: {}, beta: {}'.format('UNet', with_target, beta)
@@ -124,7 +125,7 @@ if __name__ == '__main__':
                                          shuffle=True, drop_last=False)
 
     criterion_cls = nn.KLDivLoss(reduction='batchmean')
-    optim = torch.optim.SGD(attack_net.parameters(), lr=1e-2, weight_decay=5e-3, momentum=0.9)
+    optim = torch.optim.SGD(attack_net.parameters(), lr=1e-2, weight_decay=5e-4, momentum=0.9)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optim, 'min', verbose=True, patience=20, factor=0.2, threshold=5e-3)
     if not os.path.exists(checkpoint_dir):
@@ -166,6 +167,8 @@ if __name__ == '__main__':
                 score = 0
                 gt = []
                 predictions = []
+                original_images = []
+                noise_images = []
                 for i, batch_data in tqdm.tqdm(enumerate(testing_data_loader)):
                     batch_x = batch_data[0].cuda()
                     batch_y = batch_data[1]
@@ -174,6 +177,8 @@ if __name__ == '__main__':
                     out = pretrained_model(batch_x_with_noise).detach().cpu()
                     gt.append(batch_y)
                     predictions.append(out.argmax(dim=1))
+                    original_images.append(batch_x.detach().cpu())
+                    noise_images.append(batch_x_with_noise.detach().cpu())
                     right_index = out.argmax(dim=1) == batch_y
                     wrong_index = out.argmax(dim=1) != batch_y
                     right_num += right_index.sum()
@@ -181,12 +186,23 @@ if __name__ == '__main__':
                         score += torch.sqrt((((batch_x_with_noise - batch_x).detach().cpu()[wrong_index] * 128) ** 2).mean()) * wrong_index.sum()
                 gt = torch.cat(gt).numpy()
                 predictions = torch.cat(predictions).numpy()
+                original_images = torch.cat(original_images)[: 200]
+                noise_images = torch.cat(noise_images)[: 200]
+                
+                original_images = tv.utils.make_grid(
+                    original_images, normalize=True, scale_each=True)
+                noise_images = tv.utils.make_grid(
+                    noise_images, normalize=True, scale_each=True)
+                
                 score += right_num * 128
                 score /= len(test_set)
-                scheduler.step(score)
+                scheduler.step(float(score))
                 acc = metrics.accuracy_score(predictions, gt)
                 writer.add_scalar('acc', acc, global_step=epoch)
                 writer.add_scalar('score', score, global_step=epoch)
+                if epoch % interval == 0:
+                    writer.add_image('original_images', original_images, global_step=epoch)
+                    writer.add_image('noise_images', noise_images, global_step=epoch)
             if best_acc > acc:
                 best_acc = acc
                 torch.save(attack_net.state_dict(), '{}/best_{}.pt'.format(checkpoint_dir, comment))
