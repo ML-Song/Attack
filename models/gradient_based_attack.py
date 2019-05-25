dd5a278f7f63c4a848554bdcdfc2943e6746424a
ML-Song <sl12160010@gmail.com>
2019-05-26 03:42:32 +0800

    with gradient_based_attack

--- models/gradient_based_attack.py
+++ models/gradient_based_attack.py
@@ -15,97 +15,111 @@ from utils import converter
 class AttackNet(nn.Module):
     def __init__(self, size):
         super().__init__()
-        self.noise = nn.Parameter(torch.rand(size) / 100)
+        self.noise = nn.Parameter(torch.zeros(size))
         
     def forward(self, x):
-        return self.noise + x
+        out = self.noise + x
+        out = torch.clamp(out, min=-1, max=1)
+        return out
+    
+    def renorm_grad(self, lr, levels=1/128):
+        max_grad = levels / self.noise.grad.abs().max()
+        self.noise.grad *= max_grad
+        self.noise.grad[self.noise.grad.abs() < levels / lr] = 0
         
         
 class Attack(object):
-    def __init__(self, classifier, img_size=(299, 299), transfrom=None, devices=[]):
-        self.devices = devices
-        self.img_size = img_size
+    def __init__(self, classifiers, input_size=(224, 224), output_size=(299, 299), 
+                 transfrom=None, device='cpu'):
+        self.device = device
+        self.input_size = input_size
+        self.output_size = output_size
         
         if transfrom is None:
             self.transfrom = tv.transforms.Compose([
-                tv.transforms.Resize(img_size),
+                tv.transforms.Resize(output_size),
                 tv.transforms.ToTensor(),
                 tv.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
             ])
         else:
             self.transfrom = transfrom
             
-        self.classifier_single = classifier
-        for i in self.classifier_single:
+        self.classifiers_single = classifiers
+        for i in self.classifiers_single:
             i.eval()
 
-        if len(devices) == 0:
+        if device == 'cpu':
             pass
-        elif len(devices) == 1:
-            self.classifier = [i.cuda() for i in self.classifier_single]
+        elif device == 'cuda':
+            self.classifiers = [i.cuda() for i in self.classifiers_single]
         else:
-            raise Exception('Multi Devices Not Supported!')
+            raise Exception('Device {} Not Found!'.format(device))
             
-    def get_loss(self, pred, target, noise_img, img, targeted=True, weight=64, margin=0.1):
-        target_one_hot = torch.zeros_like(pred[0])
+    def get_loss(self, preds, target, noise_img, img, targeted=True, max_perturbation=20):
+        target_one_hot = torch.zeros_like(preds[0])
         target_one_hot.scatter_(1, target.view(-1, 1), 1)
-        loss_cls = []
-#         if targeted:
-#             loss_cls = [F.cross_entropy(i, target) for i in pred]
-#         else:
-#             loss_cls = [1 - F.cross_entropy(i, target) for i in pred]
-        for p in pred:
-            prob = F.softmax(p, dim=1)
-            prob_gt = prob.gather(1, target.view(-1, 1))
-            prob_max = prob.clone()
-            prob_max[target_one_hot.type(torch.uint8)] = 0
-            prob_max = prob_max.max(dim=1)[0]
+        cls_losses = []
+        perturbation_losses = []
+        
+        for p in preds:
             if targeted:
-                prob_delta = prob_gt - prob_max - margin
+                loss_cls = F.cross_entropy(p, target, reduction='none')
+                loss_cls = loss_cls[p.argmax(dim=-1) != target].sum() / target.size(0)
             else:
-                prob_delta = prob_max - prob_gt - margin
-            loss_cls.append(-torch.clamp(prob_delta, max=0))
+                loss_cls = -F.cross_entropy(p, target, reduction='none')
+                loss_cls = loss_cls[p.argmax(dim=-1) == target].sum() / target.size(0)
+            cls_losses.append(loss_cls)
             
-        noise_img_uint8 = (noise_img * 0.5 + 0.5) * 255
-        noise_img_uint8 = F.interpolate(noise_img_uint8, self.img_size, mode='bilinear', align_corners=True)
+            noise_img_uint8 = (noise_img * 0.5 + 0.5) * 255
+            noise_img_uint8 = F.interpolate(noise_img_uint8, self.output_size, mode='bilinear', align_corners=True)
         
-        img_uint8 = (img * 0.5 + 0.5) * 255
-        img_uint8 = F.interpolate(img_uint8, self.img_size, mode='bilinear', align_corners=True)
+            img_uint8 = (img * 0.5 + 0.5) * 255
+            img_uint8 = F.interpolate(img_uint8, self.output_size, mode='bilinear', align_corners=True)
+
+            loss_perturbation = F.mse_loss(noise_img_uint8, img_uint8, reduction='none')
+            loss_perturbation = torch.sqrt(loss_perturbation.sum(dim=1))
+            loss_perturbation = loss_perturbation.mean()
+#             if targeted:
+#                 loss_perturbation = loss_perturbation[p.argmax(dim=-1) == target].sum() / target.size(0)
+#             else:
+#                 loss_perturbation = loss_perturbation[p.argmax(dim=-1) != target].sum() / target.size(0)
+            perturbation_losses.append(loss_perturbation)
+        return sum(cls_losses) / len(cls_losses), sum(perturbation_losses) / len(perturbation_losses)
         
-        loss_noise = F.mse_loss(noise_img_uint8, img_uint8)
-        loss_noise = torch.sqrt(loss_noise)
-        return sum(loss_cls) / len(loss_cls), loss_noise
-        
-    def predict(self, img, label, targeted, max_iteration=1000, margin=0.1):
+    def predict(self, img, label, targeted, max_iteration=1000, max_perturbation=20, lr=3):
         label_tensor = torch.tensor(label)
         original_x = torch.cat([self.transfrom(i).unsqueeze(dim=0) for i in img], dim=0).cuda()
         model_single = AttackNet(original_x.shape)
-        opt = torch.optim.Adam(model_single.parameters(), lr=1e-2)
+        opt = torch.optim.SGD(model_single.parameters(), lr=lr)
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(opt, max_iteration)
-        if len(self.devices) == 0:
+        if self.device == 'cpu':
             model = model_single
-        elif len(self.devices) == 1:
+        elif self.device == 'cuda':
             model = model_single.cuda()
             label_tensor = label_tensor.cuda()
         else:
-            raise Exception('Multi Devices Not Supported!')
+            raise Exception('Device {} Not Found!'.format(device))
             
-        for i in range(max_iteration):
+        for step in range(max_iteration):
             opt.zero_grad()
             out = model(original_x)
-            out = F.interpolate(out, (224, 224), mode='bilinear', align_corners=True)
-            cls = [i(out) for i in self.classifier]
-            loss_cls, loss_noise = self.get_loss(cls, label_tensor, out, original_x, targeted=targeted, margin=margin)
-            loss = loss_cls# + loss_noise
-            if i % (max_iteration / 10) == 0:
-                print([j.argmax(dim=1) for j in cls], loss_cls, loss_noise)
+            out = F.interpolate(out, self.input_size, mode='bilinear', align_corners=True)
+            cls = [i(out) for i in self.classifiers]
+            loss_cls, loss_perturbation = self.get_loss(cls, label_tensor, 
+                                                 out, original_x, targeted=targeted, 
+                                                 max_perturbation=max_perturbation)
+            loss = loss_cls + loss_perturbation
+            if step % (max_iteration / 10) == 0:
+                print([j.argmax(dim=1).data for j in cls], loss_cls.data, loss_perturbation.data)
             loss.backward()
+#             model.renorm_grad(lr=lr)
             opt.step()
-            scheduler.step(i)
+            scheduler.step(step)
+            
         noise_img = model(original_x)
         noise_img_uint8 = converter.float32_to_uint8(noise_img)
         noise_img_uint8 = noise_img_uint8.detach()
-        noise_img_uint8 = F.interpolate(noise_img_uint8, self.img_size, mode='bilinear', align_corners=True)
+        noise_img_uint8 = F.interpolate(noise_img_uint8, self.output_size, mode='bilinear', align_corners=True)
         noise_img_uint8 = noise_img_uint8.cpu().numpy().astype(np.uint8)
         noise_img_uint8 = np.transpose(noise_img_uint8, (0, 2, 3, 1))
         return noise_img_uint8
