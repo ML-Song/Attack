8b2e2e0c955708d45c606b730d73b458614caa4f
ML-Song <sl12160010@gmail.com>
2019-05-27 16:44:31 +0800

    update ddn demo

--- config_ddn.py
+++ config_ddn.py
@@ -1,10 +1,13 @@
 #coding=utf-8
 
-targeted = True
+targeted = False
 data_path = 'data/dev_data/'
 devices = [2]
-classifier_name = ['resnet50', 'vgg16', 'inceptionv4']
-classifier_path = ['saved_models/best_model_Classifier model: {} optimizer: sgd.pt'.format(i) for i in classifier_name]
+white_model_name = ['resnet50']
+white_model_path = ['saved_models/best_model_Classifier model: {} optimizer: sgd.pt'.format(i) for i in white_model_name]
+
+black_model_name = 'inceptionv4'
+black_model_path = 'saved_models/best_model_Classifier model: {} optimizer: sgd.pt'.format(black_model_name)
 
 batch_size = 16
 steps = 100
@@ -13,3 +16,5 @@ use_post_process = True
 num_classes = 110
 output_dir = 'outputs/'
 output_size = (299, 299)
+image_mean = [0.5, 0.5, 0.5]
+image_std = [0.5, 0.5, 0.5]
--- demo_ddn.py
+++ demo_ddn.py
@@ -1,30 +1,49 @@
 #coding=utf-8
 import os
 import csv
-import math
-import glob
-import tqdm
+import time
 import torch
 import numpy as np
 from PIL import Image
+import pretrainedmodels
 import torchvision as tv
 
-from models import ddn
-import pretrainedmodels
+
 from config_ddn import *
+from attack import attack
+from models.ddn import DDN
+from models.fgm_l2 import FGM_L2
+from utils.ddn_utils import NormalizedModel
 from models.classifier import ClassifierNet, Classifier
 
 
 if __name__ == '__main__':
+    start = time.time()
     os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(map(str, devices))
-    models = [pretrainedmodels.__dict__[name](pretrained=None) for name in classifier_name]
-    classifiers = [ClassifierNet(model, num_classes) for model in models]
-    for c, p in zip(classifiers, classifier_path):
-        c.load_state_dict(torch.load(p))
-        c.eval()
-        c = c.cuda()
+    image_mean = torch.tensor(image_mean).view(1, 3, 1, 1)
+    image_std = torch.tensor(image_std).view(1, 3, 1, 1)
+    
+    white_models = [pretrainedmodels.__dict__[name](pretrained=None) for name in white_model_name]
+    white_models = [ClassifierNet(model, num_classes) for model in white_models]
+    for m, p in zip(white_models, white_model_path):
+        m.load_state_dict(torch.load(p))
+        m.eval()
+        m = m.cuda()
+        m = NormalizedModel(m, image_mean, image_std)
         
+    black_model = pretrainedmodels.__dict__[black_model_name](pretrained=None)
+    black_model = ClassifierNet(black_models, num_classes)
+    black_model.load_state_dict(torch.load(black_model_path))
+    black_model.eval()
+    black_model = black_model.cuda()
+    black_model = NormalizedModel(black_model, image_mean, image_std)
         
+    def black_box_model(img):
+        t_img = torch.from_numpy(img).float().div(255).permute(2, 0, 1)
+        t_img = t_img.unsqueeze(0).to(device)
+        with torch.no_grad():
+            return black_model(t_img).argmax()
+    
     data = {'image_path': [], 'target': [], 'imgs': []}
     with open(os.path.join(data_path, 'dev.csv'), 'r') as f:
         reader = csv.DictReader(f)
@@ -37,32 +56,37 @@ if __name__ == '__main__':
             else:
                 data['target'].append(int(row['trueLabel']))
                 
-    transfrom = tv.transforms.Compose([
-        tv.transforms.Resize((224, 224)),
-        tv.transforms.ToTensor(),
-    #     tv.transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-    ])
-    attacker = ddn.DDN(steps=steps, device=torch.device('cuda: 0'))
-    epoch = math.ceil(len(data['imgs']) / batch_size)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    surrogate_models = white_models
+    attacks = [
+        DDN(steps, device=device),
+        FGM_L2(1)
+    ]
     result = []
-    for i in tqdm.tqdm(range(epoch), total=epoch):
-        torch.cuda.empty_cache()
-        batch_x = data['imgs'][i * batch_size: (i + 1) * batch_size]
-        batch_y = data['target'][i * batch_size: (i + 1) * batch_size]
+    for img, label in zip(data['imgs'], data['target']):
+        img = np.asarray(img.resize((224, 224)))
+        t_img = torch.from_numpy(img).float().div(255).permute(2, 0, 1).unsqueeze(0)
+        adv = attack(black_box_model, surrogate_models, attacks,
+                     img, label, targeted=targeted, device=device)
+        if adv is None:
+            print('Not Found')
+            result.append(None)
+            continue
+            
+        result.append(adv)
+        pred_on_adv = black_box_model(adv)
+        print('True label: {}; Prediction on the adversarial: {}'.format(label,
+                                                                         pred_on_adv))
+        l2_norm = np.sqrt(((adv - img) ** 2).sum(-1)).mean()
+        print('L2 norm of the attack: {:.4f}'.format(l2_norm))
         
-        batch_x = torch.cat([transfrom(i).unsqueeze(dim=0) for i in batch_x], dim=0).cuda()
-        batch_y = torch.tensor(batch_y).cuda()
-        
-        result.append(attacker.attack(classifiers, batch_x, labels=batch_y, 
-                                      targeted=targeted, use_post_process=use_post_process).detach().cpu().data)
-    result = torch.cat(result).numpy()
-    result = np.transpose(result, (0, 2, 3, 1))
-    result = [Image.fromarray(i) for i in (result * 255).astype(np.uint8)]
+    result = [Image.fromarray(img.astype(np.uint8)) if img is not None else data['imgs'][i] for i, img in enumerate(result)]
     if output_dir is not None:
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
         for img, path in zip(result, data['image_path']):
             name = path.split('/')[-1]
             path = os.path.join(output_dir, name)
-            img = img.resize(output_size)
             img.save(path)
+    print(time.time() - start)
+    
